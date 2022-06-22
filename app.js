e83a7ccf015d4783f98e69472b4e24b35e8d293f
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2022-06-22 20:14:16 +0200

    Cleanup

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var path = require('path');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index/index.html'));
 });
 
 app.get('/fourinarow', function(req, res) {
-    res.sendFile(path.join(__dirname + '/fourinarow.html'));
+    res.sendFile(path.join(__dirname + '/public/fourinarow/fourinarow.html'));
 });
 
 app.listen(3000);
